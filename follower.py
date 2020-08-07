793a3bf85ca2432f7be72b9e966bff2b31d8d523
Alberto Varignana <22276126+AlbertoVari@users.noreply.github.com>
2020-08-07 16:48:10 +0200

    Update follower.py

--- follower.py
+++ follower.py
@@ -1,14 +1,14 @@
 import time
 import tweepy
-auth = tweepy.OAuthHandler("MoHU91iJZEEull5B07ZICb7T2", "nZUAZ5jXkqurrnlL2TsCVaFWKrQjC5nv2TP2WqolZQ23AHtCcS")
-auth.set_access_token("722170107171090433-JBLTI5I61nq53a4x9hKzcjBmRU1Axg0", "VThOiix7pe3dvKwINKTFYhTONF6idyTSqL2jTpBt7pxpn")
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
 
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-user = api.get_user("albertovari")
+user = api.get_user("")
 
 sleeptime = 4
-pages = tweepy.Cursor(api.followers, screen_name="albertovari").pages()
+pages = tweepy.Cursor(api.followers, screen_name="").pages()
 
 while True:
     try:
@@ -20,6 +20,6 @@ while True:
     except StopIteration:
           break
     for user in page:
-         api.send_direct_message(user.id,"https://twitter.com/AlbertoVari/status/1291402267913052162?s=20")
+         api.send_direct_message(user.id,"")
          print(user.screen_name,user.id)
 
